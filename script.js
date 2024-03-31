12512fc4b9829854adda4d4e668da5863694a6ba
jpydilshan <yureshdilshan5316@gmail.com>
2024-04-01 02:45:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// script.js
-const playButton = document.getElementById('yt');
+
+const videoButton = document.getElementById('yt');
 const videoModal = document.getElementById('video-modal');
 
-playButton.addEventListener('click', () => {
+videoButton.addEventListener('click', () => {
     videoModal.style.display = 'block';
 });
 
 // Close the modal when clicking outside the video or on the modal itself
 document.addEventListener('click', (event) => {
-    if (event.target === videoModal || event.target === playButton) {
+    if (event.target === videoModal || event.target === videoButton) {
         videoModal.style.display = 'none';
     }
 });
